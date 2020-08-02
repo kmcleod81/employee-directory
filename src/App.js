f8cdbb084d65c49ef893f957f8a3b1b3e1b13d07
kdawg <mcleod.karla@gmail.com>
2020-08-02 12:01:11 -0400

    added axios and utils folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
+import 'bootstrap/dist/css/bootstrap.css';
+
 
 function App() {
   return (
